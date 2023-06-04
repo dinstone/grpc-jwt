db150e0a4d9cc8b60aabaccd3b64c42ee65971c6
dinstone <dinstone@163.com>
2023-06-04 09:32:17 +0800

    update netty to native

--- grpc-server/src/main/java/com/dinstone/grpc/server/GrpcServiceServer.java
+++ grpc-server/src/main/java/com/dinstone/grpc/server/GrpcServiceServer.java
@@ -9,36 +9,37 @@ import com.google.protobuf.StringValue;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.ServerInterceptors;
+import io.grpc.ServerServiceDefinition;
 import io.grpc.stub.StreamObserver;
 
 public class GrpcServiceServer {
 
-    public static final class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
-        @Override
-        public void sayHello(StringValue request, StreamObserver<StringValue> responseObserver) {
-            String clientId = AuthConstant.AUTH_CLIENT_ID.get();
-            responseObserver
-                    .onNext(StringValue.newBuilder().setValue(clientId + " say hello:" + request.getValue()).build());
-            responseObserver.onCompleted();
-        }
-    }
+	public static final class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
+		@Override
+		public void sayHello(StringValue request, StreamObserver<StringValue> responseObserver) {
+			String clientId = AuthConstant.AUTH_CLIENT_ID.get();
+			responseObserver
+					.onNext(StringValue.newBuilder().setValue(clientId + " say hello:" + request.getValue()).build());
+			responseObserver.onCompleted();
+		}
+	}
 
-    public static void main(String[] args) {
-        Server server = ServerBuilder.forPort(9898)
-                .addService(ServerInterceptors.intercept(new HelloServiceImpl(), new AuthInterceptor())).build();
+	public static void main(String[] args) {
+		ServerServiceDefinition serviceDefinition = ServerInterceptors.intercept(new HelloServiceImpl(), new AuthInterceptor());
+		Server server = ServerBuilder.forPort(9898).addService(serviceDefinition).build();
 
-        try {
-            System.out.println("server on 9898");
-            server.start().awaitTermination();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (server != null) {
-                server.shutdownNow();
-            }
-        }
-    }
+		try {
+			System.out.println("server on 9898");
+			server.start().awaitTermination();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (server != null) {
+				server.shutdownNow();
+			}
+		}
+	}
 
 }
